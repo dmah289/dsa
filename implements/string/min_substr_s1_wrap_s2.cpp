1c6a1311e9662c225e4f07180c8d98b3fd1f3502
dmah <manhnd@ikameglobal.com>
2026-02-27 18:31:47 +0700

    sync

--- implements/string/min_substr_s1_wrap_s2.cpp
+++ implements/string/min_substr_s1_wrap_s2.cpp
@@ -21,31 +21,39 @@ int main(){
     int t; cin >> t;
     while(t--){
         string s1, s2; cin >> s1 >> s2;
+
         int freq1[256] = {0}, freq2[256] = {0};
         for(char c : s2) freq2[c]++;
-        int cnt = 0;
-        int left = 0, len = INT_MAX, idx = -1;
+
+        int s2CharAmountAppearedInS1 = 0;
+        int left = 0, currLen = INT_MAX, idx = -1;
+
         for(int i = 0; i < s1.length(); i++){
             freq1[s1[i]]++;
-            if(freq1[s1[i]] <= freq2[s1[i]]) cnt++;
-            if(cnt == s2.length()){
+            if(freq1[s1[i]] <= freq2[s1[i]]) s2CharAmountAppearedInS1++;
+
+            if(s2CharAmountAppearedInS1 == s2.length()){
                 while(freq1[s1[left]] > freq2[s1[left]] || freq2[s1[left]] == 0){
-                    if(freq1[s1[left]] > freq2[s1[left]]) freq1[s1[left]]--;
+                    if(freq1[s1[left]] > freq2[s1[left]])
+                        freq1[s1[left]]--;
+
                     left++;
                 }
-                if(len > i - left + 1){
-                    len = i - left + 1;
+
+                if(currLen > i - left + 1){
+                    currLen = i - left + 1;
                     idx = left;
                 }
-                if(freq1[s1[left]] == freq2[s1[left]]) {
+
+                if(freq1[s1[left]] == freq2[s1[left]]){
                     freq1[s1[left]]--;
                     left++;
-                    cnt--;
+                    s2CharAmountAppearedInS1--;
                 }
             }
         }
         if(idx == -1) cout << "-1\n";
-        else cout << s1.substr(idx, len) << endl;
+        else cout << s1.substr(idx, currLen) << endl;
     }
 
     return 0;
